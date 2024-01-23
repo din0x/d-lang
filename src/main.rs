b72fc382f7a42260dcfbfe6ea0de22c8af5569d0
din0x <superrobertxd52@gmail.com>
2024-01-23 17:29:19 +0100

    Fix main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod compiler;
 mod runtime;
 
+use crate::runtime::Scope;
+
 fn main() {
     let code = r###"hello = 1"###;
 
     let result = compiler::compile(code);
 
     if let Ok(expr) = result {
-        let v = runtime::eval(expr);
+        let mut scope = Scope::default();
+        let v = runtime::eval(expr, &mut scope);
         println!("{}", v);
     } else if let Err(err) = result {
         println!("{}", err)
