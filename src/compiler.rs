87f1790ff8d59563b2741a2636d1174b0824168e
din0x <superrobertxd52@gmail.com>
2024-01-28 14:02:23 +0100

    Re-export Block

--- src/compiler.rs
+++ src/compiler.rs
@@ -4,7 +4,7 @@ mod typing;
 
 use std::fmt::Display;
 
-pub use parser::{Assignment, BinOperator, Expr, ExprInfo, ExprKind, IfExpr, VariableDeclaration};
+pub use parser::{Assignment, BinOperator, Expr, ExprInfo, ExprKind, IfExpr, VariableDeclaration, Block};
 
 pub use typing::Scope;
 
